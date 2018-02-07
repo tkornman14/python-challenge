fa8f14e8d0d7b15105da0ee5a243a4540cb41ef2
Taylor Kornman <taylor.kornman@gmail.com>
2018-02-06 17:34:00 -0800

    added homework3

---
+++ Homework3PyBoss.py
@@ -0,0 +1,12 @@
+import employee_data1.csv
+import employee_data2.csv
+
+employee_data2.csv = employee_data2.csvReader
+employee_data2.csvReader = employee_data2.split ("Emp ID,First Name,Last Name,DOB,SSN,State")
+employee_data2
+for row in csvReader ("Emp ID,First Name,Last Name,DOB,SSN,State")
+
+
+for row in csvReader:
+
+            
